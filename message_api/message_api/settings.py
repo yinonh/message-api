ba26dc0c4dd28c65204bb79c9b87bfdc064162c0
yinonh <yinon.h21@gmail.com>
2022-11-03 13:32:44 +0200

    SECRET_KEY changed

--- message_api/message_api/settings.py
+++ message_api/message_api/settings.py
@@ -28,7 +28,7 @@ if os.path.isfile(dotenv_file):
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$qu(miy3(9jyzgygt3n2op)2xwbgyj!chcx8^a8-1kj_y1ef(p"
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
