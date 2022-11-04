0473a5822f972e20a835b8c2d44262bbe84e6b7b
yinonh <yinon.h21@gmail.com>
2022-11-04 11:57:36 +0200

    post method changed

--- message_api/Message/views.py
+++ message_api/Message/views.py
@@ -8,7 +8,7 @@ from rest_framework.response import Response
 import datetime
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework.decorators import api_view
+from rest_framework.decorators import action
 
 
 class MessagesList(APIView):
@@ -29,8 +29,8 @@ class MessagesList(APIView):
                                  'message': openapi.Schema(type=openapi.TYPE_STRING)
                              },
                          ),
-                         operation_description='Uninstall a version of Site')
-    @api_view(['POST'])
+                         operation_description='Send new message')
+    @action(detail=True, methods=['POST'])
     def post(self, request, format=None):
         request.data['sender'] = request.user.id
         request.data['creation_date'] = datetime.date.today()
